12fea2d26ee74af7400a8550e0c089e4dbc96f65
amelie <amelieka@gmx.de>
2023-04-18 12:44:37 +0200

    add calculation and test text

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,16 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println(addition(4,5));
+        System.out.println(addDisplay(addition(5,6)));
+
+    }
+
+    public static int addition(int a, int b){
+        return a+b;
+    }
+
+    public static String addDisplay(int c){
+        return ("Die Summe beträgt: " + c);
     }
 }
