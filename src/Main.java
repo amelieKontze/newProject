86ce5050a3126b52d492256e35e9a2aaa8351cb0
amelie <amelieka@gmx.de>
2023-04-18 11:16:49 +0200

    add calculation and test text

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Test Text");
+
+        int num1 = 4;
+        int num2 = 5;
+
+        System.out.println(num1 + num2);
+
     }
 }
